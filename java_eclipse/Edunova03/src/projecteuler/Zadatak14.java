043b4095dbf6132cd571b04f81b5f30003cbaab1
iva-i <ivaivezic95@gmail.com>
2021-01-04 11:47:32 +0100

    dodani zadatci u projecteuler

--- java_eclipse/Edunova03/src/projecteuler/Zadatak14.java
+++ java_eclipse/Edunova03/src/projecteuler/Zadatak14.java
@@ -5,47 +5,39 @@ import java.util.Arrays;
 public class Zadatak14 {
 
 	public static void main(String[] args) {
-		// The following iterative sequence is defined for the set of positive integers:
+		// The following iterative niz is defined for the set of positive integers:
 		// n → n/2 (n is even)
 		// n → 3n + 1 (n is odd)
 		// Using the rule above and starting with 13, we generate the following
-		// sequence:
+		// niz:
 		// 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-		// It can be seen that this sequence (starting at 13 and finishing at 1)
+		// It can be seen that this niz (starting at 13 and finishing at 1)
 		// contains 10 terms. Although it has not been proved yet (Collatz Problem), it
-		// is thought that all starting numbers finish at 1.
-		// Which starting number, under one million, produces the longest chain?
+		// is thought that all starting brojs finish at 1.
+		// Which starting broj, under one million, produces the longest chain?
 		// NOTE: Once the chain starts the terms are allowed to go above one million.
-		
-		
-		int[] niz=new int[500];
-		
-			for(int i=0; i<20; i++) {
-				niz[0]=i;
-				for(int j=0; j<499;j++) {
-					if(niz[j]%2==0 && niz[j]!=1) {
-						niz[j+1]=niz[j]/2;
-					}else if(niz[j]%2!=0 && niz[j]!=1) {
-						niz[j+1]=3*niz[j]+1;
-					}
-				}
-				System.out.println(Arrays.toString(niz));
-			}
-			
-			int brojac=0;
-			
-//			for(int i=0; i<=500) {
-//				if(niz[i]!=0) {
-//					brojac++
-//				}else {
-//					
-//				}
-//			}
-			
-			
-		
 
-		
-		
+		long duljina = 0, pocetak = 0;
+		long niz;
+
+		for (int i = 2; i <= 1000000; i++) {
+			int dulj = 1;
+			niz = i;
+			while (niz != 1) {
+				if ((niz % 2) == 0) {
+					niz = niz / 2;
+				} else {
+					niz = niz * 3 + 1;
+				}
+				dulj++;
+			}
+
+			if (dulj > duljina) {
+				duljina = dulj;
+				pocetak = i;
+			}
+
+		}
+
 	}
 }
