96def10f46e8a312bb9c475077ac6c28b71ebfe4
iva-i <ivaivezic95@gmail.com>
2021-01-22 12:23:28 +0100

    uspjela iskoristiti konstante iz Fan u FanTest, minimalne izmjene

--- java_learningOOP/src/tasks/Fan.java
+++ java_learningOOP/src/tasks/Fan.java
@@ -3,9 +3,9 @@ package tasks;
 class Fan {
 	
 	// data fields
-	final int SLOW = 1;
-	final int MEDIUM = 2;
-	final int FAST = 3;
+	public static final int SLOW = 1;
+	public static final int MEDIUM = 2;
+	public static final int FAST = 3;
 	private int speed = SLOW;
 	private boolean on = false;
 	private double radius = 5;
--- java_learningOOP/src/tasks/FanTest.java
+++ java_learningOOP/src/tasks/FanTest.java
@@ -14,12 +14,12 @@ public class FanTest {
 		Fan fan1 = new Fan();
 		Fan fan2 = new Fan();
 		
-		fan1.setSpeed(3);
+		fan1.setSpeed(Fan.FAST);
 		fan1.setRadius(10);
 		fan1.setColor("yellow");
 		fan1.setOn(true);
 		
-		fan2.setSpeed(2);
+		fan2.setSpeed(Fan.MEDIUM);
 		
 		System.out.println(fan1.toString());
 		System.out.println(fan2.toString());
