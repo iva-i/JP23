34a3663d3f9fdfbae8eda59288f3d2315c262c9c
iva-i <ivaivezic95@gmail.com>
2021-01-22 11:42:33 +0100

    dodan zadatak

---
+++ java_learningOOP/src/tasks/Location.java
@@ -0,0 +1,37 @@
+package tasks;
+
+class Location {
+
+	// data fields
+	public int row, column;
+	public double maxValue;
+
+	// constructor
+	public Location(int row, int column, double maxValue) {
+		super();
+		this.row = row;
+		this.column = column;
+		this.maxValue = maxValue;
+	}
+
+	public static Location locateLargest(double[][] a) {
+
+		double largest = 0;
+		int row = 0, column = 0;
+
+		// going through rows
+		for (int i = 0; i < a.length; i++) {
+			// going through columns
+			for (int j = 0; j < a[0].length; j++) {
+				if (a[i][j] > largest) {
+					largest = a[i][j];
+					row = i;
+					column = j;
+				}
+			}
+		}
+
+		return new Location(row, column, largest);
+	}
+
+}
---
+++ java_learningOOP/src/tasks/LocationTest.java
@@ -0,0 +1,37 @@
+package tasks;
+
+import java.util.*;
+
+public class LocationTest {
+
+	public static void main(String[] args) {
+		/**
+		 * Write a test program that prompts the user to enter a two-dimensional array
+		 * and displays the location of the largest element in the array.
+		 */
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter number of rows and columns: ");
+
+		int row = input.nextInt(), column = input.nextInt();
+
+		double[][] array2D = new double[row][column];
+
+		System.out.println("Enter values into 2D array: ");
+
+		for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D[0].length; j++) {
+				array2D[i][j] = input.nextDouble();
+			}
+		}
+
+		input.close();
+
+		Location loc = Location.locateLargest(array2D);
+
+		System.out.printf("The location of the largest element %f is at (%d,%d).", loc.maxValue, loc.row, loc.column);
+
+	}
+
+}
