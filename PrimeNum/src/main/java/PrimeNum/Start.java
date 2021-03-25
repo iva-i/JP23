786e06728878e721a0d6c768d5c5efdc325aec6e
iva-i <ivaivezic95@gmail.com>
2021-03-25 12:50:09 +0100

    izradila mini app za provjeru je li neki broj prost

---
+++ PrimeNum/src/main/java/PrimeNum/Start.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrimeNum;
+
+import PrimeNumView.Menu;
+
+/**
+ *
+ * @author Skec
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        
+        new Menu().setVisible(true);
+        
+    }
+    
+}
