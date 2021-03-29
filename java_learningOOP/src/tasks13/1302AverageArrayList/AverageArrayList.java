fb32cd04394118a095a16f5d389ea850426bc822
iva-i <ivaivezic95@gmail.com>
2021-03-29 18:25:59 +0200

    dodan zadatak

---
+++ java_learningOOP/src/tasks13/1302AverageArrayList/AverageArrayList.java
@@ -0,0 +1,53 @@
+package tasks13;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageArrayList {
+	
+	public AverageArrayList() {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter integers into the list (0 to finish): ");
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		Integer help = 1;
+		
+		while(help != 0) {
+			try {
+				help = input.nextInt();
+				list.add(help);
+			} catch (Exception e) {
+				System.out.println("Enter integer!");
+			}
+		}
+		
+		input.close();
+		
+		average(list);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		new AverageArrayList();
+		
+	}
+	
+	public static void average(ArrayList<Integer> list) {
+		
+		Integer sum = 0;
+		
+		for(int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		
+		Integer average = sum / list.size();
+		
+		System.out.println("Average is: " + average);
+		
+	}
+
+}
